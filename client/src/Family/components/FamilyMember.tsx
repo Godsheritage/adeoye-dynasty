d66cc8e8f4ec4946069bf978cba002639545aa6a
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-05-29 21:22:10 +0100

    updates

--- client/src/Family/components/FamilyMember.tsx
+++ client/src/Family/components/FamilyMember.tsx
@@ -4,13 +4,19 @@ import { useNavigate } from "react-router-dom";
 import Card from "../../shared/UI Elements/card/Card";
 
 const FamilyMember: React.FC<familyMemberTypes> = ({ member }) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="family-member">
       <Card>
-        <img src={member.image} alt={member.name} onClick={() => navigate(`/family/${member.name}`)} />
-        <p className=" text-dark">{member.name}</p>
-        <p className=" text-dark">{member.age}</p>
+        <img
+          src={member.image}
+          alt={member.name}
+          onClick={() => navigate(`/family/${member.name}`)}
+        />
+        <div className="d-flex flex-column justify-content-around align-items-around">
+          <p className=" text-dark">{member.name}</p>
+          <p className=" text-dark">Age:{member.age}</p>
+        </div>
       </Card>
     </div>
   );
