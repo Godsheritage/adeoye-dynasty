1edcaafd4ae69b436cfc38f0e7ef545f161f0aa6
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-06-03 22:30:47 +0100

    dding amazon s3

--- server/src/s3.ts
+++ server/src/s3.ts
@@ -1,1 +1,31 @@
-import S3 from 'aws-sdk/clients/s3'
+import S3 from "aws-sdk/clients/s3";
+import dotenv from "dotenv";
+import fs from "fs";
+
+dotenv.config();
+
+const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+});
+
+//upload an image
+
+export const uploadFile: any = (file: any) => {
+  const fileStream = fs.createReadStream(file.path);
+  const uploadParams: any = {
+    Bucket: AWS_BUCKET_NAME,
+    Body: fileStream,
+    Key: file.filename,
+  };
+
+  return s3.upload(uploadParams).promise();
+};
+
+//download an image
