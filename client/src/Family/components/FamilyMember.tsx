9d880ca48f4ae7390a95224d21e7a48d5a15ba10
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-07-23 19:56:29 +0100

    making UI updates

--- client/src/Family/components/FamilyMember.tsx
+++ client/src/Family/components/FamilyMember.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { contextTypes } from "../../types";
 import { familyMemberTypes } from "../../types";
-import { Link } from "react-router-dom";
 import Card from "../../shared/UI Elements/card/Card";
 import FamilyContext from "../../context/FamilyContext";
+import img from "../../shared/pictures/Crownfit.jpg";
 
 const FamilyMember: React.FC<familyMemberTypes> = ({ member }) => {
   return (
     <div className="family-member">
-      <Card>
+      <Card className = "m-3">
         <Link to={`/family/${member.name}`}>
-          <img src={`/images/${member.image}`} alt={member.name} />
+          {/* <img src={`/images/${member.image}`} alt={member.name} />  */}
+          <img src={img} alt={member.name} className="img-fluid" />
           <div className="d-flex flex-column pt-2 justify-content-around align-items-around">
             <p className=" text-dark">{member.name}</p>
             <p className=" text-dark">Age:{member.age}</p>
@@ -22,4 +24,4 @@ const FamilyMember: React.FC<familyMemberTypes> = ({ member }) => {
   );
 };
 
-export default FamilyMember;
+export default FamilyMember;  
