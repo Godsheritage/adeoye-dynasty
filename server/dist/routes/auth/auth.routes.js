2f862680c6205b13af06c36b9778e1d7061eb328
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-08-23 06:47:36 +0100

    commenting on updataes

--- server/dist/routes/auth/auth.routes.js
+++ server/dist/routes/auth/auth.routes.js
@@ -4,8 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const auth_controllers_1 = __importDefault(require("./auth.controllers"));
 const express_validator_1 = require("express-validator");
+const auth_controllers_1 = __importDefault(require("./auth.controllers"));
 const authRoute = express_1.default.Router();
-authRoute.post('/', [(0, express_validator_1.check)("username").notEmpty(), (0, express_validator_1.check)("password").isLength({ min: 8 }).custom((val) => /[^A-za-z0-9\s]/g.test(val))], auth_controllers_1.default);
+//TO SIGN IN USERS
+authRoute.post("/", [
+    (0, express_validator_1.check)("username").notEmpty(),
+    (0, express_validator_1.check)("password")
+        .isLength({ min: 8 })
+        .custom((val) => /[^A-za-z0-9\s]/g.test(val)),
+], auth_controllers_1.default);
 exports.default = authRoute;
--- server/src/routes/auth/auth.routes.ts
+++ server/src/routes/auth/auth.routes.ts
@@ -1,9 +1,19 @@
-import express from 'express'
-import httpSignInUsers from './auth.controllers'
-import {check} from 'express-validator'
+import express from "express";
+import { check } from "express-validator";
+import httpSignInUsers from "./auth.controllers";
 
-const authRoute = express.Router()
+const authRoute = express.Router();
 
-authRoute.post('/', [check("username").notEmpty(), check("password").isLength({min:8}).custom((val) => /[^A-za-z0-9\s]/g.test(val))], httpSignInUsers )
+//TO SIGN IN USERS
+authRoute.post(
+  "/",
+  [
+    check("username").notEmpty(),
+    check("password")
+      .isLength({ min: 8 })
+      .custom((val) => /[^A-za-z0-9\s]/g.test(val)),
+  ],
+  httpSignInUsers
+);
 
-export default authRoute
+export default authRoute;
